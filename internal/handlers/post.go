b8c17feb2cd7cd2f37ee103965fdf01e60f015af
Mohammadamin Yaghoubi <yaghoubi.mn@gmail.com>
2025-02-10 12:54:38 +0330

    add post CRUD

--- internal/handlers/post.go
+++ internal/handlers/post.go
@@ -1,1 +1,180 @@
 package handlers
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yaghoubi-mn/voter/internal/dtos"
+	"github.com/yaghoubi-mn/voter/internal/enums"
+	"github.com/yaghoubi-mn/voter/internal/models"
+	"github.com/yaghoubi-mn/voter/internal/services"
+	"github.com/yaghoubi-mn/voter/pkg/response"
+)
+
+type PostHandler interface {
+	Create(c *gin.Context)
+	Update(c *gin.Context)
+	Delete(c *gin.Context)
+	GetAll(c *gin.Context)
+	GetByID(c *gin.Context)
+}
+
+type postHandler struct {
+	service  services.PostService
+	response response.JsonResponse
+}
+
+func NewPostHandler(service services.PostService, response response.JsonResponse) PostHandler {
+	return &postHandler{
+		service:  service,
+		response: response,
+	}
+}
+
+func (h *postHandler) Create(c *gin.Context) {
+	var postInput dtos.PostInput
+
+	decoder := json.NewDecoder(c.Request.Body)
+	defer c.Request.Body.Close()
+	decoder.DisallowUnknownFields()
+	decoder.Decode(&postInput)
+
+	user, ok := c.Get("user")
+	if !ok {
+		h.response.ServerErrorResponse(c, errors.New("user not found in context"))
+		return
+	}
+	responseDTO := h.service.Create(postInput, user.(models.User))
+	if responseDTO.ServerErr != nil || responseDTO.UserErrs != nil {
+		h.response.ServerOrUserErrorResponse(c, responseDTO.ServerErr, responseDTO.UserErrs, responseDTO.ResponseCode)
+		return
+	}
+
+	h.response.Response(c, http.StatusOK, responseDTO.ResponseCode, responseDTO.Data)
+}
+
+func (h *postHandler) Update(c *gin.Context) {
+	var postInput dtos.PostInput
+
+	decoder := json.NewDecoder(c.Request.Body)
+	defer c.Request.Body.Close()
+	decoder.DisallowUnknownFields()
+	decoder.Decode(&postInput)
+
+	user, ok := c.Get("user")
+	if !ok {
+		h.response.ServerErrorResponse(c, errors.New("user not found in context"))
+		return
+	}
+
+	// get postId from url
+	postIdString, ok := c.Params.Get("postId")
+	if !ok {
+		h.response.ErrorResponse(c, http.StatusBadRequest, "post_id_not_found_in_url", nil, errors.New("post_id: post id not found in url params"))
+		return
+	}
+	postId, err := strconv.Atoi(postIdString)
+	if err != nil {
+		h.response.ErrorResponse(c, http.StatusBadRequest, "invalid_post_id", nil, errors.New("post_id: invalid post id"))
+		return
+	}
+
+	responseDTO := h.service.Update(postInput, uint64(postId), user.(models.User))
+	if responseDTO.ServerErr != nil || responseDTO.UserErrs != nil {
+		h.response.ServerOrUserErrorResponse(c, responseDTO.ServerErr, responseDTO.UserErrs, responseDTO.ResponseCode)
+		return
+	}
+
+	h.response.Response(c, http.StatusOK, responseDTO.ResponseCode, responseDTO.Data)
+}
+
+func (h *postHandler) Delete(c *gin.Context) {
+
+	user, ok := c.Get("user")
+	if !ok {
+		h.response.ServerErrorResponse(c, errors.New("user not found in context"))
+		return
+	}
+
+	// get postId from url
+	postIdString, ok := c.Params.Get("postId")
+	if !ok {
+		h.response.ErrorResponse(c, http.StatusBadRequest, "post_id_not_found_in_url", nil, errors.New("post_id: post id not found in url params"))
+		return
+	}
+	postId, err := strconv.Atoi(postIdString)
+	if err != nil {
+		h.response.ErrorResponse(c, http.StatusBadRequest, "invalid_post_id", nil, errors.New("post_id: invalid post id"))
+		return
+	}
+
+	responseDTO := h.service.Delete(uint64(postId), user.(models.User))
+	if responseDTO.ServerErr != nil || responseDTO.UserErrs != nil {
+		h.response.ServerOrUserErrorResponse(c, responseDTO.ServerErr, responseDTO.UserErrs, responseDTO.ResponseCode)
+		return
+	}
+
+	h.response.Response(c, http.StatusOK, responseDTO.ResponseCode, responseDTO.Data)
+}
+
+func (h *postHandler) GetAll(c *gin.Context) {
+
+	// get query params from url
+	pageString := c.Query("page")
+	if pageString == "" {
+		h.response.ErrorResponse(c, http.StatusBadRequest, "page_not_found_in_url", nil, errors.New("page: page is required. ex: /?page=1"))
+		return
+	}
+	page, err := strconv.Atoi(pageString)
+	if err != nil {
+		h.response.ErrorResponse(c, http.StatusBadRequest, "invalid_page", nil, errors.New("page: invalid page. page must be integer"))
+		return
+	}
+
+	orderByString := c.Query("order_by")
+
+	var orderBy enums.SortBy
+	switch orderByString {
+	case "score":
+		orderBy = enums.SortByScore
+	case "date":
+		orderBy = enums.SortByDate
+	default:
+		orderBy = enums.DefaultSort
+	}
+
+	// call service
+	responseDTO := h.service.GetAll(orderBy, page)
+	if responseDTO.ServerErr != nil || responseDTO.UserErrs != nil {
+		h.response.ServerOrUserErrorResponse(c, responseDTO.ServerErr, responseDTO.UserErrs, responseDTO.ResponseCode)
+		return
+	}
+
+	h.response.Response(c, http.StatusOK, responseDTO.ResponseCode, responseDTO.Data)
+}
+
+func (h *postHandler) GetByID(c *gin.Context) {
+
+	// get postId from url
+	postIdString, ok := c.Params.Get("postId")
+	if !ok {
+		h.response.ErrorResponse(c, http.StatusBadRequest, "post_id_not_found_in_url", nil, errors.New("post_id: post id not found in url params"))
+		return
+	}
+	postId, err := strconv.Atoi(postIdString)
+	if err != nil {
+		h.response.ErrorResponse(c, http.StatusBadRequest, "invalid_post_id", nil, errors.New("post_id: invalid post id"))
+		return
+	}
+
+	responseDTO := h.service.GetByID(uint64(postId))
+	if responseDTO.ServerErr != nil || responseDTO.UserErrs != nil {
+		h.response.ServerOrUserErrorResponse(c, responseDTO.ServerErr, responseDTO.UserErrs, responseDTO.ResponseCode)
+		return
+	}
+
+	h.response.Response(c, http.StatusOK, responseDTO.ResponseCode, responseDTO.Data)
+}
--- internal/services/post.go
+++ internal/services/post.go
@@ -1,1 +1,171 @@
 package services
+
+import (
+	"errors"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/yaghoubi-mn/voter/internal/custom_errors"
+	"github.com/yaghoubi-mn/voter/internal/dtos"
+	"github.com/yaghoubi-mn/voter/internal/enums"
+	"github.com/yaghoubi-mn/voter/internal/models"
+	"github.com/yaghoubi-mn/voter/internal/repositories"
+)
+
+type PostService interface {
+	Create(postInput dtos.PostInput, user models.User) dtos.ResponseDTO
+	Update(postInput dtos.PostInput, postId uint64, user models.User) dtos.ResponseDTO
+	Delete(postId uint64, user models.User) dtos.ResponseDTO
+	GetAll(sortBy enums.SortBy, page int) dtos.ResponseDTO
+	GetByID(postId uint64) dtos.ResponseDTO
+}
+
+type postService struct {
+	repo     repositories.PostRepository
+	validate *validator.Validate
+}
+
+func NewPostService(repo repositories.PostRepository, validate *validator.Validate) PostService {
+	return &postService{
+		repo:     repo,
+		validate: validate,
+	}
+}
+
+func (s *postService) Create(postInput dtos.PostInput, user models.User) (responseDTO dtos.ResponseDTO) {
+
+	responseDTO.Data = make(map[string]any)
+
+	errs := s.validate.Struct(postInput)
+	if errs != nil {
+		errList := make([]error, 0, 2)
+		for _, err := range errs.(validator.ValidationErrors) {
+			errList = append(errList, errors.New(err.StructField()+": "+err.Error()))
+		}
+
+		responseDTO.ResponseCode = "invalid_field"
+		responseDTO.UserErrs = errList
+		return
+	}
+
+	// save post to database
+	var post models.Post
+	post.Title = postInput.Title
+	post.Content = postInput.Content
+	post.AuthorID = user.ID
+
+	if err := s.repo.Create(post); err != nil {
+		responseDTO.ServerErr = err
+		return
+	}
+
+	responseDTO.Data["msg"] = "post created"
+	return
+}
+
+func (s *postService) Update(postInput dtos.PostInput, postId uint64, user models.User) (responseDTO dtos.ResponseDTO) {
+	responseDTO.Data = make(map[string]any)
+
+	errs := s.validate.Struct(postInput)
+	if errs != nil {
+		errList := make([]error, 0, 2)
+		for _, err := range errs.(validator.ValidationErrors) {
+			errList = append(errList, errors.New(err.StructField()+": "+err.Error()))
+		}
+
+		responseDTO.ResponseCode = "invalid_field"
+		responseDTO.UserErrs = errList
+		return
+	}
+
+	// check post blongs to user
+	var post models.Post
+	post, err := s.repo.GetByID(postId)
+	if err != nil {
+		responseDTO.ServerErr = err
+		return
+	}
+
+	if post.AuthorID != user.ID {
+		responseDTO.UserErrs = []error{errors.New("you havn't access to this post")}
+		responseDTO.ResponseCode = "access_denied"
+		return
+	}
+
+	// update post
+	post.Title = postInput.Title
+	post.Content = postInput.Content
+	err = s.repo.Update(post)
+	if err != nil {
+		if err == custom_errors.RecordNotFound {
+			responseDTO.UserErrs = []error{errors.New("post not found")}
+			responseDTO.ResponseCode = "not_found"
+			return
+		}
+		responseDTO.ServerErr = err
+		return
+	}
+
+	responseDTO.Data["msg"] = "Done"
+	return
+
+}
+
+func (s *postService) Delete(postId uint64, user models.User) (responseDTO dtos.ResponseDTO) {
+	responseDTO.Data = make(map[string]any)
+
+	// check post blongs to user
+	post, err := s.repo.GetByID(postId)
+	if err != nil {
+		responseDTO.ServerErr = err
+		return
+	}
+
+	if post.AuthorID != user.ID {
+		responseDTO.UserErrs = []error{errors.New("you havn't access to this post")}
+		responseDTO.ResponseCode = "access_denied"
+		return
+	}
+
+	// delete post
+	err = s.repo.Delete(postId)
+	if err != nil {
+		if err == custom_errors.RecordNotFound {
+			responseDTO.UserErrs = []error{errors.New("post not found")}
+			responseDTO.ResponseCode = "not_found"
+			return
+		}
+		responseDTO.ServerErr = err
+		return
+	}
+
+	responseDTO.Data["msg"] = "Done"
+	return
+}
+
+func (s *postService) GetAll(sortBy enums.SortBy, page int) (responseDTO dtos.ResponseDTO) {
+	responseDTO.Data = make(map[string]any)
+
+	// get data from database
+	posts, err := s.repo.GetAll(sortBy, page)
+	if err != nil {
+		responseDTO.ServerErr = err
+		return
+	}
+
+	responseDTO.Data["data"] = posts
+	return
+}
+
+func (s *postService) GetByID(postId uint64) (responseDTO dtos.ResponseDTO) {
+	responseDTO.Data = make(map[string]any)
+
+	post, err := s.repo.GetByID(postId)
+	if err != nil {
+		responseDTO.ServerErr = err
+		return
+	}
+
+	postOutput := dtos.GetPostOutputFromPost(post)
+	responseDTO.Data["data"] = postOutput
+	return
+}
