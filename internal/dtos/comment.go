ff941d971fd17963f9571f4d190bdfdae0e906d7
ruhollah82 <0robena0@gmail.com>
2026-06-17 11:40:50 +0330

    backend: expose deleted state on comment output and preserve deleted comment rendering

--- internal/dtos/comment.go
+++ internal/dtos/comment.go
@@ -19,17 +19,26 @@ type CommentOutput struct {
 	AuthorID       uint64 `json:"author_id"`
 	AuthorUsername string `json:"author_username"`
 	ParentID       uint64 `json:"parent_id"`
+	Deleted        bool   `json:"deleted"`
 }
 
 func GetCommentOutputFromComment(comment models.Comment) CommentOutput {
+	authorUsername := comment.Author.Username
+	content := comment.Content
+	if comment.IsDeleted {
+		authorUsername = "[deleted]"
+		content = "[deleted]"
+	}
+
 	return CommentOutput{
 		ID:             comment.ID,
-		Content:        comment.Content,
+		Content:        content,
 		Score:          comment.Score,
 		CreatedAt:      comment.CreatedAt.Format(config.TimeFormat),
 		ModifiedAt:     comment.ModifiedAt.Format(config.TimeFormat),
 		AuthorID:       comment.AuthorID,
-		AuthorUsername: comment.Author.Username,
+		AuthorUsername: authorUsername,
 		ParentID:       comment.ParentID,
+		Deleted:        comment.IsDeleted,
 	}
 }
