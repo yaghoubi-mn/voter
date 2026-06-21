67a65a869a9bc8d196eb634c197c8c26607278a2
Mohammadamin Yaghoubi <yaghoubi.mn@gmail.com>
2026-06-21 19:55:10 +0330

    increase comments content limit

--- internal/models/post.go
+++ internal/models/post.go
@@ -5,7 +5,7 @@ import "time"
 type Post struct {
 	ID      uint64
 	Title   string `gorm:"size:200;not null"`
-	Content string `gorm:"size:1000;not null"`
+	Content string `gorm:"size:10000;not null"`
 
 	Score         int       `gorm:"default:0"`
 	Views         uint64    `gorm:"default:0"`
